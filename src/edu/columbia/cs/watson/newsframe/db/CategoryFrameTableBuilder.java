2d677e61967aeb182f5fb178fa7f429dee3614ca
kedz <perl4.068>
2013-05-15 17:29:44 -0400

    Made category frame table builder.

--- src/edu/columbia/cs/watson/newsframe/db/CategoryFrameTableBuilder.java
+++ src/edu/columbia/cs/watson/newsframe/db/CategoryFrameTableBuilder.java
@@ -24,14 +24,14 @@ public class CategoryFrameTableBuilder {
 
             Connection connection = ConnectionFactory.getConnection();
 
-            PreparedStatement selectAll = connection.prepareStatement("SELECT entity1, entity2, path, count, ngram FROM sem_frame");
+            PreparedStatement selectAll = connection.prepareStatement("SELECT entity1, entity2, path, count, ngram FROM high_frames");
             ResultSet resultSet = selectAll.executeQuery();
 
             while(resultSet.next()) {
 
-                String ent1 = resultSet.getString(1).intern();
-                String ent2 = resultSet.getString(2).intern();
-                String path = resultSet.getString(3).intern();
+                String ent1 = resultSet.getString(1);
+                String ent2 = resultSet.getString(2);
+                String path = resultSet.getString(3);
                 int count = resultSet.getInt(4);
                 int ngram = resultSet.getInt(5);
 
@@ -39,20 +39,20 @@ public class CategoryFrameTableBuilder {
                 HashSet<String> cat2Set = new HashSet<String>();
 
 
-                PreparedStatement ent1CatsQuery = connection.prepareStatement("SELECT category FROM category_map where entity = ?");
+                PreparedStatement ent1CatsQuery = connection.prepareStatement("SELECT category FROM high_category_map where entity = ?");
                 ent1CatsQuery.setString(1,ent1);
                 ResultSet ent1Cats = ent1CatsQuery.executeQuery();
                 while(ent1Cats.next()) {
-                    cat1Set.add(ent1Cats.getString(1).intern());
+                    cat1Set.add(ent1Cats.getString(1));
                 }
                 ent1Cats = null;
                 ent1CatsQuery = null;
 
-                PreparedStatement ent2CatsQuery = connection.prepareStatement("SELECT category FROM category_map where entity = ?");
+                PreparedStatement ent2CatsQuery = connection.prepareStatement("SELECT category FROM high_category_map where entity = ?");
                 ent2CatsQuery.setString(1,ent2);
                 ResultSet ent2Cats = ent2CatsQuery.executeQuery();
                 while(ent2Cats.next()) {
-                    cat2Set.add(ent2Cats.getString(1).intern());
+                    cat2Set.add(ent2Cats.getString(1));
                 }
                 ent2Cats = null;
                 ent1CatsQuery = null;
@@ -62,7 +62,7 @@ public class CategoryFrameTableBuilder {
                     for(String cat2 : cat2Set) {
 
                         PreparedStatement insertToCatFrame = connection.prepareStatement(
-                                "INSERT INTO cat_frame (category1, category2, path, count, ngram)\n"+
+                                "INSERT INTO cat_frame_reduce (category1, category2, path, count, ngram)\n"+
                                 "VALUES (?, ?, ?, ?, ?)\n"+
                                 "ON DUPLICATE KEY UPDATE count = count + " + count);
                         insertToCatFrame.setString(1,cat1);
